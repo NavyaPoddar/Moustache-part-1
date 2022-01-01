20ee580e5b2d029136fa84ddaa326622585322b5
NavyaPoddar <84183648+NavyaPoddar@users.noreply.github.com>
2022-01-01 15:07:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+    stache=loadImage("Moustache.gif");
+}
+
+function setup(){
+    canvas= createCanvas(700,500);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function download(){
+    save("Image.png");
+}
